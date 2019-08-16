6a3446b23984883210f14db7ae94a68c5f053bdf
nadia arcese <nadiaarcese@gmail.com>
2019-08-16 16:55:53 -0400

    added comments and fixed functions

--- Desktop/Angular/TTSfirstapp/someFile.js
+++ Desktop/Angular/TTSfirstapp/someFile.js
@@ -1,9 +1,27 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function sayHello(person) {
     return "Hello, " + person;
 }
 var user = "Super Student";
-document.getElementById("para").innerHTML = sayHello(user);
+function thingy() {
+    document.getElementById("para").innerHTML = sayHello(user);
+}
+;
+//creating a person object with full name and contact info
 var Person = /** @class */ (function () {
+    //creating a constructor for the attributes of person object
     function Person(firstName, lastName, age, phone, state, zipCode, occupation) {
         this.FirstName = firstName;
         this.LastName = lastName;
@@ -13,14 +31,36 @@ var Person = /** @class */ (function () {
         this.ZipCode = zipCode;
         this.Occupation = occupation;
     }
+    //method to return full name of person
     Person.prototype.fullName = function () {
         return "Full name: " + this.FirstName + this.LastName;
     };
+    //method to introduce person with full name, age, home, and job.
     Person.prototype.quickIntro = function () {
         return "Meet" + this.FirstName + this.LastName + "." + "They are " + this.Age + "and live in " + this.State + ". " + "Their job is " + this.Occupation + ".";
     };
+    //method to return full name and phone number
     Person.prototype.contactInfo = function () {
         return "You can reach" + this.FirstName + this.LastName + "at " + this.Phone;
     };
     return Person;
 }());
+//creating people 
+var jim = new Person("Jim", "Halpert", 32, 1111111111, "PA", "18503", "Salesperson");
+var pam = new Person("Pam", "Beasley", 30, 2222222222, "PA", "18503", "Receptionist");
+var angela = new Person("Angela", "Martin", 34, 3333333333, "PA", "18503", "Accountant");
+var dwight = new Person("Dwight", "Schrute", 35, 4444444444, "PA", "18503", "Assistant Regional Manager");
+//using person object to create employee object with added hourly wage
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(FirstName, LastName, Age, Phone, State, ZipCode, Occupation, hourlyWage) {
+        return _super.call(this, FirstName, LastName, Age, Phone, State, ZipCode, Occupation) || this;
+    }
+    return Employee;
+}(Person));
+//y: number; 
+//WeeklyWage: number;
+//function to calculate weekly earnings
+function WeeklyWage(hourlyWage, y) {
+    return hourlyWage * y;
+}
--- Desktop/Angular/TTSfirstapp/someFile.ts
+++ Desktop/Angular/TTSfirstapp/someFile.ts
@@ -4,7 +4,10 @@ function sayHello(person: string): string {
 
 var user = "Super Student";
 
-document.getElementById("para").innerHTML = sayHello(user);
+function thingy (){
+    document.getElementById("para").innerHTML = sayHello(user)};
+
+//creating a person object with full name and contact info
 class Person {
     FirstName: string;
     LastName: string;
@@ -14,6 +17,7 @@ class Person {
     ZipCode: string;
     Occupation: string;
 
+//creating a constructor for the attributes of person object
     constructor(firstName: string, lastName: string, age: number,phone: number, state: string, zipCode: string, occupation: string) {
         this.FirstName = firstName;
         this.LastName = lastName;
@@ -23,36 +27,41 @@ class Person {
         this.ZipCode = zipCode;
         this.Occupation = occupation;
     }
+    //method to return full name of person
     fullName(){
         return "Full name: " + this.FirstName + this.LastName; 
     }
+    //method to introduce person with full name, age, home, and job.
     quickIntro(){
         return "Meet" + this.FirstName + this.LastName + "." + "They are " + this.Age + "and live in " + this.State + ". " + "Their job is " + this.Occupation + ".";
     }
+    //method to return full name and phone number
     contactInfo(){
         return "You can reach" + this.FirstName + this.LastName + "at " + this.Phone;
     }
 }
-
+//creating people 
 var jim = new Person("Jim", "Halpert", 32, 111_111_1111, "PA", "18503","Salesperson");
 var pam = new Person("Pam", "Beasley", 30, 222_222_2222, "PA", "18503","Receptionist");
 var angela = new Person("Angela", "Martin", 34, 333_333_3333, "PA","18503","Accountant"); 
 var dwight = new Person("Dwight", "Schrute", 35, 444_444_4444, "PA", "18503", "Assistant Regional Manager"); 
 
-
+//using person object to create employee object with added hourly wage
 class Employee extends Person {
     constructor(
-        FirstName: string;
-        LastName: string;
-        Age: number;
-        Phone: number;
-        State: string;
-        ZipCode: string;
-        Occupation: string;
-        hourlyWage: number;) 
+        FirstName: string,
+        LastName: string,
+        Age: number,
+        Phone: number,
+        State: string,
+        ZipCode: string,
+        Occupation: string,
+        hourlyWage: number) 
     {super(FirstName, LastName, Age, Phone, State, ZipCode, Occupation);}
-     
-    HourlyWage(){
-
     }
-}
+    //y: number; 
+    //WeeklyWage: number;
+    //function to calculate weekly earnings
+    function WeeklyWage(hourlyWage: number, y: number): number{
+       return hourlyWage * y;}
+    
